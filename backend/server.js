f98072fac0abd5e9493638cc4e50ec48f5b322d7
amarmeshram567 <meshramamar567@gmail.com>
2026-01-14 22:13:22 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 const port = process.env.PORT || 5000
 
 const allowedOrigins = [
-    'http://localhost:5173'
+    'https://gig-flow-assignment-client.vercel.app'
 ]
 
 app.use(cors({
